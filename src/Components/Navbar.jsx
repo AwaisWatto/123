d3a87bf9a4b01540d06cbd301acd4a0e9d633084
Awais <awaiswatto88@gmail.com>
2022-12-20 20:23:48 +0500

    second push

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,22 +2,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {  Link } from "react-router-dom";
 import { Navbar, Nav, Form } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
-
+import './Navbar.css';
 
 
 function Navbar1() {
   return (
     <div>
     <Navbar bg="grey" expand="lg">
-      <Navbar> <Link to="/">SiteName</Link></Navbar>
+      <Navbar> <Link className='pink' to="/">SiteName</Link></Navbar>
       
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Col md={{ span: 3, offset: 3 }}> 
         <Nav className='me-auto '>
 
-          <Nav.Link> <Link to="/requesttable">Blog</Link></Nav.Link>
-          <Nav.Link ><Link to="/About-Us">About Us</Link></Nav.Link>
+          <Nav.Link> <Link className='pink' to="/requesttable">Blog</Link></Nav.Link>
+          <Nav.Link ><Link className='pink' to="/About-Us">About Us</Link></Nav.Link>
           <Nav.Link ><Link to="/Contact-US">Contact Us</Link></Nav.Link>
         </Nav>
         </Col>
@@ -27,6 +27,7 @@ function Navbar1() {
         <Nav className="me-auto ">
           <Nav.Link><Link to="/Cart">Cart</Link></Nav.Link>
           <Nav.Link><Link to="/User">User Profile</Link></Nav.Link>
+          <Nav.Link><Link to="/">UP</Link></Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
